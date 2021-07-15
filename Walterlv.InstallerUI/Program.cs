6e3c9a30ac2ec87e40abefb6f804b7a1af61007e
walterlv <xblero@gmail.com>
2021-07-15 16:38:58 +0800

    Add WPF UI.

---
+++ Walterlv.InstallerUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walterlv.InstallerUI
+{
+    public class Program
+    {
+        private static int Main(string[] args)
+        {
+            // 这里的代码仅为调试使用，在最终的项目中无任何用途。
+            var app = new App();
+            return app.Run();
+        }
+    }
+}
